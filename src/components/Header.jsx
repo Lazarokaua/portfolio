0666ba0c9554298e5bf506d34d97ac4f60d4e91f
Lazarokaua <lazarokaua4@gmail.com>
2024-12-29 21:42:37 -0300

    feat(header): criando todo o header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/logo_lk-removebg-preview 1.png"
+import "../styles.css"
+import { Nav } from "./Nav"
+
+export function Header() {
+    return (
+        <>
+            <header className="bg-white text-gray-300 px-6 py-4">
+                <div className="flex justify-between items-center max-w-5xl mx-auto">
+                    {/* <!-- Nome --> */}
+                     <a href="#"><img src={logo} className="w-28"/></a>
+
+                    {/* <!-- Links --> */}
+                    <Nav />
+                </div>
+
+                {/* <!-- Linha fina abaixo --> */}
+                <div className="border-t border-gray-500 mt-2"></div>
+            </header>
+
+        </>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,37 @@
+import us from "../assets/US.jpg"
+import br from "../assets/BR.png"
+
+import { useState } from "react"
+
+export function Nav() {
+
+    const [language, setLanguage] = useState("en")
+
+    const navTexts = {
+        en: { contact: "Contact", projetcs: "Projects", about: "About" },
+        pt: { contact: "Contato", projetcs: "Projetos", about: "Sobre" },
+    }
+
+    return (
+        <>
+            <nav className="flex space-x-4 text-sm">
+                <a href="#" className="hover:underline">{navTexts[language].contact}</a>
+                <a href="#" className="hover:underline">{navTexts[language].projetcs}</a>
+                <a href="#" className="hover:underline">{navTexts[language].about}</a>
+
+                <button title="Click to change language" onClick={() => {
+                    setLanguage((e) => (e === "en" ? "pt" : "en"))
+                }}>
+                    {language === "en" ? (
+                        <img src={us} className="w-6 cursor-pointer" alt="US Flag" />
+                    ) : (
+                        <img src={br} className="w-6 cursor-pointer" alt="BR Flag" />
+                    )}
+
+                </button>
+
+
+            </nav>
+        </>
+    )
+}
